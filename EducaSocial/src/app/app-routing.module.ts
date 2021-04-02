e3479206a39d524d2134d365f211be545c871eeb
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-02 18:39:55 -0300

    create group component

--- EducaSocial/src/app/app-routing.module.ts
+++ EducaSocial/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedPostagemComponent } from './feed-postagem/feed-postagem.component';
+import { GroupComponent } from './group/group.component';
 
 const routes: Routes = [
   {
     'path': 'posts',
     component: FeedPostagemComponent
+  }, 
+  {
+    'path': 'grupo-usuario',
+    component: GroupComponent
   }
 ];
 
--- EducaSocial/src/app/app.module.ts
+++ EducaSocial/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EntrarCadastroComponent } from './entrar-cadastro/entrar-cadastro.component';
 import { HomeUsuarioComponent } from './home-usuario/home-usuario.component';
 import { FeedPostagemComponent } from './feed-postagem/feed-postagem.component';
+import { GroupComponent } from './group/group.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EntrarCadastroComponent,
-    HomeUsuarioComponent,
-    FeedPostagemComponent
+   HomeUsuarioComponent,
+    FeedPostagemComponent,
+    GroupComponent
   ],
   imports: [
     BrowserModule,
