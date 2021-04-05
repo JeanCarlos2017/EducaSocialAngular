0c42ae121868a7a629fb3d50148172db7de0d4a9
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-05 09:56:32 -0300

    criando as rotas para entrar e cadastrar, além de consertar a rota de 'home-grupo'

--- EducaSocial/src/app/entrar/entrar.component.ts
+++ EducaSocial/src/app/entrar/entrar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+
+  entrar(){
+    this.router.navigate(['/home-usuario/posts']);
+  }
 }
