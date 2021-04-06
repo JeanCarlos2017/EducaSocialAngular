2714699397d02aaf3177c718e726544db4d39565
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-06 14:40:49 -0300

    Update home-usuario.component.ts
    titulo e método de deslogar automático

--- EducaSocial/src/app/home-usuario/home-usuario.component.ts
+++ EducaSocial/src/app/home-usuario/home-usuario.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-home-usuario',
@@ -12,12 +14,22 @@ export class HomeUsuarioComponent implements OnInit {
   url_foto:string;
   codigo_usuario: number;
   nome_usuario: string;
-  constructor(public router: Router) { }
+  constructor( private router: Router, 
+            private authService: AuthService, 
+             private title: Title) { }
 
   ngOnInit(): void {
+    window.scroll(0,0);
+    this.title.setTitle("Bem vindo ao EducaSocial");
+
+    //ambiente de procudação
     this.url_foto= environment.foto;
     this.codigo_usuario= environment.codigo_usuario;
     this.nome_usuario= environment.nome;
+
+    if(this.authService.logado()){
+      this.router.navigate(['/entrar']);
+    }
   }
 
 }
