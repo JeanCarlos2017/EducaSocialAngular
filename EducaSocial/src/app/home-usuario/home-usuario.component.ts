e7ab9184f9282940c27e29ee83e93b58ed98f748
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-06 14:28:29 -0300

    informações dos usuários

--- EducaSocial/src/app/home-usuario/home-usuario.component.ts
+++ EducaSocial/src/app/home-usuario/home-usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-home-usuario',
@@ -7,10 +8,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./home-usuario.component.css']
 })
 export class HomeUsuarioComponent implements OnInit {
-
+ 
+  url_foto:string;
+  codigo_usuario: number;
+  nome_usuario: string;
   constructor(public router: Router) { }
 
   ngOnInit(): void {
+    this.url_foto= environment.foto;
+    this.codigo_usuario= environment.codigo_usuario;
+    this.nome_usuario= environment.nome;
   }
 
 }
