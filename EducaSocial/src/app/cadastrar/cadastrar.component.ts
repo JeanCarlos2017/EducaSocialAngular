7854ae6087f5f220bdd4fd2bb362be6ff5f3dfca
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-07 12:10:23 -0300

    Update cadastrar.component.ts

--- EducaSocial/src/app/cadastrar/cadastrar.component.ts
+++ EducaSocial/src/app/cadastrar/cadastrar.component.ts
@@ -30,6 +30,7 @@ export class CadastrarComponent implements OnInit {
     if(this.user.senha == this.confirmarSenha){
       this.authService.cadastrar(this.user).subscribe((resp: User) => {
         this.user= resp;
+        this.user= new User();
         this.router.navigate(['/entrar']);
         alert('Usuário cadastrado com sucesso');
       });
