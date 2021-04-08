5c1c71b01f8853d0644ed68242824b6970fdf429
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-08 15:40:23 -0300

    model Tema e Postagem

---
+++ EducaSocial/src/app/models/Postagem.ts
@@ -0,0 +1,14 @@
+import { Comentario } from "./Comentario";
+import { Tema } from "./Tema";
+import { User } from "./user";
+
+export class Postagem{
+    id_postagem: number;
+	usuario: User;
+	titulo: string;
+	data_postagem: Date;
+	imagem: string;
+	saldo_reacoes: number;
+	temaList: Tema[];
+	comentarioList: Comentario[];
+}
